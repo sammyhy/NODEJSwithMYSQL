90544143927f4df3c61c27498fa39fd479325b2a
sammy <sammykkoon@gmail.com>
2021-08-08 01:14:21 +0900

    학습 진행

---
+++ README.md
@@ -0,0 +1,6 @@
+# NODEJSwithMYSQL
+
+## NODEJS 책 학습
+
+### nodejs와 mysql(sequelize 이용) 이용하여 CRUD 학습
+
---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+const {sequelize} = require('./models');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
+
+const app = express();
+app.set('port',process.env.PORT || 3000);
+app.set('view engine', 'html');
+nunjucks.configure('views',{
+    express : app,
+    watch : true
+});
+
+sequelize.sync({force : false}).then(()=>{
+    console.log('database connect success')
+}).catch((err)=>{
+    console.error(err)
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use('/',indexRouter);
+app.use('/users',usersRouter);
+app.use('/comments',commentsRouter);
+
+app.use((req, res, next)=>{
+    const error = new Error(`${req.method} ${req.url} router does not exist`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next)=>{
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production'? err:{};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'),()=>{
+    console.log(app.get('port'),'번 포트 대기중');
+});
